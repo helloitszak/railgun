48c8b2e6e6ac125c2b9a11ceb1a620c9367ee799
Zak Kristjanson <ubercow1337@gmail.com>
2013-09-14 23:40:04 -0700

    Whoops, reserved column name

--- db/migrate/002_create_torrents.rb
+++ db/migrate/002_create_torrents.rb
@@ -1,7 +1,7 @@
 class CreateTorrents < ActiveRecord::Migration
 	def change
 		create_table :torrents do |t|
-			t.string :hash
+			t.string :hash_string
 			t.datetime :name
 			t.boolean :copied, default: false
 		end
