0585351f573d737b23ff74cd1b54a328d4ee43a9
Zak Kristjanson <ubercow1337@gmail.com>
2013-09-15 01:31:09 -0700

    Should be a string, not a datetime

--- db/migrate/002_create_torrents.rb
+++ db/migrate/002_create_torrents.rb
@@ -2,7 +2,7 @@ class CreateTorrents < ActiveRecord::Migration
 	def change
 		create_table :torrents do |t|
 			t.string :hash_string
-			t.datetime :name
+			t.string :name
 			t.boolean :copied, default: false
 		end
 	end
